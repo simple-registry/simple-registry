9b1ea0461a8760922e6d2de4f8f86bc8c7b34f2c
Philippe Chepy <philippe@innosens.lu>
2024-10-14 20:16:05 +0200

    Move tracing setup to specific module

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,6 @@ use hyper::{Method, Request, Response, StatusCode};
 use hyper_util::rt::tokio::TokioIo;
 use lazy_static::lazy_static;
 use notify::{recommended_watcher, Event, RecommendedWatcher, RecursiveMode, Watcher};
-use opentelemetry::trace::TracerProvider as _;
-use opentelemetry::{global, KeyValue};
-use opentelemetry_sdk::trace::{BatchConfig, RandomIdGenerator, Sampler, Tracer, TracerProvider};
-use opentelemetry_sdk::{runtime, Resource};
-use opentelemetry_semantic_conventions::{
-    attribute::{DEPLOYMENT_ENVIRONMENT_NAME, SERVICE_NAME, SERVICE_VERSION},
-    SCHEMA_URL,
-};
-use opentelemetry_stdout as stdout;
 use regex::Regex;
 use registry::Registry;
 use serde::{Deserialize, Serialize};
@@ -28,10 +19,7 @@ use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite};
 use tokio::net::TcpListener;
 use tokio::pin;
 use tokio_rustls::TlsAcceptor;
-use tracing::{debug, error, info, instrument, Level};
-use tracing_opentelemetry::OpenTelemetryLayer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
+use tracing::{debug, error, info, instrument};
 use uuid::Uuid;
 use x509_parser::prelude::{FromDer, X509Certificate};
 
@@ -44,6 +32,7 @@ mod policy;
 mod registry;
 mod storage;
 mod tls;
+mod tracing_helper;
 
 use self::http::request::{parse_authorization_header, parse_query_parameters, parse_range_header};
 use self::http::response::paginated_response;
@@ -180,75 +169,12 @@ fn tls_watcher_event_handler(event: Result<Event, notify::Error>) {
     }
 }
 
-// TODO: Tracing setup: to be moved in more appropriate location
-fn resource() -> Resource {
-    Resource::from_schema_url(
-        [
-            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
-            KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
-            KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, "develop"),
-        ],
-        SCHEMA_URL,
-    )
-}
-
-fn init_tracer(sampling_rate: f64) -> Tracer {
-    let provider = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_trace_config(
-            opentelemetry_sdk::trace::Config::default()
-                .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
-                    sampling_rate,
-                ))))
-                .with_id_generator(RandomIdGenerator::default())
-                .with_resource(resource()),
-        )
-        .with_batch_config(BatchConfig::default())
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
-        .install_batch(runtime::Tokio)
-        .unwrap();
-
-    global::set_tracer_provider(provider.clone());
-    provider.tracer("tracing-otel-subscriber")
-}
-
-fn setup_tracing() {
-    let config = CONFIG
-        .load()
-        .observability
-        .clone()
-        .and_then(|observability| observability.tracing.clone());
-
-    if let Some(tracing) = config {
-        let tracer = init_tracer(tracing.sampling_rate);
-
-        tracing_subscriber::registry()
-            .with(tracing_subscriber::filter::LevelFilter::from_level(
-                Level::INFO,
-            ))
-            .with(tracing_subscriber::fmt::layer())
-            .with(OpenTelemetryLayer::new(tracer))
-            .init();
-    } else {
-        tracing_subscriber::registry()
-            .with(tracing_subscriber::filter::LevelFilter::from_level(
-                Level::INFO,
-            ))
-            .with(tracing_subscriber::fmt::layer())
-            .init();
-    }
-}
-
 #[tokio::main]
 async fn main() -> io::Result<()> {
     reload_config()?;
     reload_tls()?;
 
-    let _ = TracerProvider::builder()
-        .with_simple_exporter(stdout::SpanExporter::default())
-        .build();
-
-    setup_tracing();
+    tracing_helper::setup_tracing();
 
     let mut config_watcher = recommended_watcher(config_watcher_event_handler).map_err(|err| {
         error!("Failed to create watcher: {}", err);
---
+++ src/tracing_helper.rs
@@ -0,0 +1,76 @@
+use crate::CONFIG;
+use opentelemetry::trace::TracerProvider as _;
+use opentelemetry::{global, KeyValue};
+use opentelemetry_sdk::trace::{BatchConfig, RandomIdGenerator, Sampler, Tracer, TracerProvider};
+use opentelemetry_sdk::{runtime, Resource};
+use opentelemetry_semantic_conventions::{
+    attribute::{DEPLOYMENT_ENVIRONMENT_NAME, SERVICE_NAME, SERVICE_VERSION},
+    SCHEMA_URL,
+};
+use opentelemetry_stdout as stdout;
+use tracing::Level;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+fn resource() -> Resource {
+    Resource::from_schema_url(
+        [
+            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+            KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+            KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, "develop"),
+        ],
+        SCHEMA_URL,
+    )
+}
+
+fn init_tracer(sampling_rate: f64) -> Tracer {
+    let provider = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_trace_config(
+            opentelemetry_sdk::trace::Config::default()
+                .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+                    sampling_rate,
+                ))))
+                .with_id_generator(RandomIdGenerator::default())
+                .with_resource(resource()),
+        )
+        .with_batch_config(BatchConfig::default())
+        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
+        .install_batch(runtime::Tokio)
+        .unwrap();
+
+    global::set_tracer_provider(provider.clone());
+    provider.tracer("tracing-otel-subscriber")
+}
+
+pub fn setup_tracing() {
+    let _ = TracerProvider::builder()
+        .with_simple_exporter(stdout::SpanExporter::default())
+        .build();
+
+    let config = CONFIG
+        .load()
+        .observability
+        .clone()
+        .and_then(|observability| observability.tracing.clone());
+
+    if let Some(tracing) = config {
+        let tracer = init_tracer(tracing.sampling_rate);
+
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::filter::LevelFilter::from_level(
+                Level::INFO,
+            ))
+            .with(tracing_subscriber::fmt::layer())
+            .with(OpenTelemetryLayer::new(tracer))
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::filter::LevelFilter::from_level(
+                Level::INFO,
+            ))
+            .with(tracing_subscriber::fmt::layer())
+            .init();
+    }
+}
